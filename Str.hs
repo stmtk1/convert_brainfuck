c0f3eb607b9e5fa9e765774cb94c4d12bd495026
斎藤基樹 <stmtk@saitoumotoki-no-MacBook-Pro.local>
2018-05-03 22:23:09 +0900

    first commit

---
+++ Str.hs
@@ -0,0 +1,6 @@
+module Str(str) where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote
+
+str = QuasiQuoter { quoteExp = stringE }
---
+++ brain_c.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main where
+import Control.Applicative
+import Str
+
+before_main = [str|
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+list<char> tape;
+list<char>::iterator position;
+
+void incl(){
+    (*position)++;
+}
+
+void decl(){
+    (*position)--;
+}
+
+void next_var(){
+    if(next(position) == tape.end()){
+        tape.push_back(0);
+    }
+    position++;
+}
+
+void prev_var(){
+    if(position == tape.begin()){
+        tape.push_front(0);
+    }
+    position--;
+}
+
+void puts(){
+    cout << *position;
+}
+
+void gets(){
+    cin >> *position;
+} |]
+
+convert_brainfuck :: String -> String
+convert_brainfuck "" = ""
+convert_brainfuck ('+':rest) = "incl();\n" ++ convert_brainfuck rest
+convert_brainfuck ('-':rest) = "decl();\n" ++ convert_brainfuck rest
+convert_brainfuck ('>':rest) = "next_var();\n" ++ convert_brainfuck rest
+convert_brainfuck ('<':rest) = "prev_var();\n" ++ convert_brainfuck rest
+convert_brainfuck ('.':rest) = "puts();\n" ++ convert_brainfuck rest
+convert_brainfuck (',':rest) = "gets();\n" ++ convert_brainfuck rest
+convert_brainfuck ('[':rest) = "while(*position != 0){\n" ++ convert_brainfuck rest
+convert_brainfuck (']':rest) = "}\n" ++ convert_brainfuck rest
+
+create_main :: String -> String
+create_main input = "int main(){tape.push_back(0);\nposition = tape.begin();\n" ++ (convert_brainfuck input) ++ "cout << endl;return 0;}"
+
+create_program :: String -> String
+create_program input = before_main ++ (create_main input)
+
+brainfuck :: String
+brainfuck = ">+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.[-]>++++++++[<++++>-]<.>+++++++++++[<+++++>-]<.>++++++++[<+++>-]<.+++.------.--------.[-]>++++++++[<++++>-]<+.[-]++++++++++."
+--brainfuck = ">+++++++++[<++++++++>-]<."
+
+main = putStrLn $ create_program brainfuck
