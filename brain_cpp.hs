597285602e14f707c3d52cee925f7c9cd4e06f6d
stmtk1 <13043069@gakushuin.ac.jp>
2018-05-31 08:04:38 +0900

    cpp implements changed

--- brain_cpp.hs
+++ brain_cpp.hs
@@ -6,58 +6,80 @@ import Str
 
 before_main = [str|
 #include <iostream>
-#include <string>
-#include <list>
 
 using namespace std;
 
-list<char> tape;
-list<char>::iterator position;
+int len;
+unique_ptr<char> tape;
+char *head;
 
-void incl(){
-    (*position)++;
+void init(){
+    len = 1000;
+    tape = unique_ptr<char>(new char[len]);
+    char *new_tape = tape.get();
+    head = &new_tape[0];
+    for(int i = 0; i < len; i++)
+        new_tape[i] = 0;
 }
 
-void decl(){
-    (*position)--;
+void increment(){
+    (*head)++;
+}
+
+void decrement(){
+    (*head)--;
 }
 
 void next_var(){
-    if(next(position) == tape.end()){
-        tape.push_back(0);
+    char *tape_inner = tape.get();
+    head++;
+    if(&tape_inner[len] <= head){
+        len += 1000;
+        char *new_tape = new char[len];
+        for(int i = 0; i < len; i++){
+            new_tape[i] = i < len - 1000 ? tape_inner[i] : 0;
+        }
+        tape = unique_ptr<char>(new_tape);
     }
-    position++;
 }
 
 void prev_var(){
-    if(position == tape.begin()){
-        tape.push_front(0);
+    char *tape_inner = tape.get();
+    head--;
+    if(*head < 0){
+        len += 1000;
+        head += 1000;
+        char *new_tape = new char[len];
+        for(int i = 0; i < len; i++){
+            new_tape[i] = i < 1000 ? 0 : tape_inner[i];
+        }
+        tape = unique_ptr<char>(new_tape);
     }
-    position--;
 }
 
-void puts(){
-    cout << *position;
+void print_var(){
+    cout << *head;
 }
 
-void gets(){
-    cin >> *position;
-} |]
+void get_var(){
+    cin >> *head;
+}
+|]
 
 convert_brainfuck :: String -> String
 convert_brainfuck "" = ""
-convert_brainfuck ('+':rest) = "incl();\n" ++ convert_brainfuck rest
-convert_brainfuck ('-':rest) = "decl();\n" ++ convert_brainfuck rest
+convert_brainfuck ('+':rest) = "increment();\n" ++ convert_brainfuck rest
+convert_brainfuck ('-':rest) = "decrement();\n" ++ convert_brainfuck rest
 convert_brainfuck ('>':rest) = "next_var();\n" ++ convert_brainfuck rest
 convert_brainfuck ('<':rest) = "prev_var();\n" ++ convert_brainfuck rest
-convert_brainfuck ('.':rest) = "puts();\n" ++ convert_brainfuck rest
-convert_brainfuck (',':rest) = "gets();\n" ++ convert_brainfuck rest
-convert_brainfuck ('[':rest) = "while(*position != 0){\n" ++ convert_brainfuck rest
+convert_brainfuck ('.':rest) = "print_var();\n" ++ convert_brainfuck rest
+convert_brainfuck (',':rest) = "get_var();\n" ++ convert_brainfuck rest
+convert_brainfuck ('[':rest) = "while(*head != 0){\n" ++ convert_brainfuck rest
 convert_brainfuck (']':rest) = "}\n" ++ convert_brainfuck rest
 convert_brainfuck (_:rest) = convert_brainfuck rest
 
 create_main :: String -> String
-create_main input = "int main(){tape.push_back(0);\nposition = tape.begin();\n" ++ (convert_brainfuck input) ++ "cout << endl;return 0;}"
+create_main input = "int main(){init();\n" ++ (convert_brainfuck input) ++ "return 0;}"
 
 create_program :: String -> String
 create_program input = before_main ++ (create_main input)
